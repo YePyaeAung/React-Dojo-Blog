d68402f323aeccfc32ffa0af33d524e21c3fdd48
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-15 23:00:55 +0630

    Create Navbar and Home Components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to new blog';
-  const likes = 50;
-  // const person = { name:"Ye Pyae", age: 23 };
-  const link = "https://www.google.com";
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked  { likes } times.</p>
-        {/* <p>{ person }</p>
-        <p>{ person.name }</p> */}
-
-        <p>{ 20 }</p>
-        <p>{ "hello world" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random()*10 }</p>
-        <a href={ link } target="_blank">Google Site</a>
+        <Home/>
       </div>
     </div>
   );
 }
 
 export default App;
-
-// Can't Output the object
