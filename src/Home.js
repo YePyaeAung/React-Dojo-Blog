a7db12f7298aae6c60f5d8a7e9b55170884f9c66
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-16 19:48:53 +0630

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("hello react", e);
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Ye Pyae', e)}>Click Me Again</button>
         </div>
     )
 }
