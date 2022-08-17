c1bd039047d5e1200528af2f6998d52760d433e5
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-17 17:34:36 +0630

    Functions as Props(delete an item)

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,16 @@ const Home = () => {
         { title : 'My new website', body : 'Lorem ipsum...', author : 'mario', id : 1},
         { title : 'Welcome party!', body : 'Lorem ipsum...', author : 'john', id : 2},
         { title : 'Web dev top tips', body : 'Lorem ipsum...', author : 'mario', id : 3}
-    ])
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter( blog => blog.id !== id );
+        setBlogs(newBlogs);
+    }
+    
     return (
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs!"/>
-            <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title="Mario's Blogs!"/>
+            <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete }/>
         </div>
     )
 }
