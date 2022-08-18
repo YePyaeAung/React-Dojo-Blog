4d7acc09d091d3dc448b1422a20db9269649ce12
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-08-18 23:07:33 +0630

    React Router Dom (Version 6)

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,16 +1,17 @@
-import { useHistory, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
-    const history = useHistory();
+    const navigate = useNavigate();
+    
 
     const handleDelete = () => {
         fetch('http://localhost:8000/blogs/' + blog.id, {
             method : "DELETE"
         }).then(() => {
-            history.push('/');
+            navigate('/');
         })
     }
     return (
